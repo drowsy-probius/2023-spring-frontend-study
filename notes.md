96912c28ecac1a999cac471abdaaa6377a1fba0c
drowsy-probius <k123s456h@gmail.com>
2023-05-10 20:30:45 +0900

    0510

--- notes.md
+++ notes.md
@@ -111,3 +111,122 @@ const k = 'asdf' // type: 'asdf'
 
 
 TODO: react.dev의 challenge
+
+# 0510
+
+상호작용과 그에 따른 side effect (좋은거, 변화가 생겨야 그게 상호작용이고 피드백이니까)
+
+## Event on Browser
+
+사용자와의 상호작용과 DOM에 관한 이벤트
+
+Event Handler가 이벤트를 처리
+
+[event bubbling, capturing](https://developer.mozilla.org/ko/docs/Learn/JavaScript/Building_blocks/Events)
+
+
+```js
+event.preventDefault();
+event.stopPropagation();
+```
+
+`this` 5가지 동작
+
+`1st citizen` 1급객체 
+
+
+
+
+## Adding Interactivity
+
+### Responding to Events
+
+### State: A Component's Memor
+관리해야 할 데이터, 시간에 따라 변하는 데이터 
+
+`useState` (cf. `useEffect`)
+
+### Render and Commit
+
+component가 어떻게 표시되는가
+
+리컨실레이션
+
+
+Trigering -> Rendering -> Committing
+
+
+render
+- initial 될 때 -> root component 호출
+- state가 변경될 때 (setState -> trigger -> re-render. update component). 상태가 변해서 trigger된 컴포넌트 함수 호출
+
+VirtualDOM이라서 변경된 일부분만 re-rendering 가능.
+
+commit
+- 실제로 DOM 변경 
+
+
+cf. critical rendering path (브라우저의 렌더링 방식, 또는 painting)
+
+
+
+### State as a Snapshot
+
+state는 상태에 따라 변하니까 그 때의 state 
+
+Your component returns a snapshot of the UI with a fresh set of props and event handlers in its JSX, all calculated using the state values from that render!
+
+상태 변경해도 리렌더링 전에는 이전 상태 값을 참조하고 있다. 다음 렌더링 전에는 (렌더링과 렌더링 사이에서는) 변한 상태 값이 반영된다. 
+
+```js
+setNumber(number + 1);
+setNumber(number + 1);
+setNumber(number + 1);
+```
+스냅샷되어 들어가기 때문에 (number + 1)만 실행됨.
+
+
+### Queueing a Series of State Updates
+React waits until all code in the event handlers has run before processing your state updates. 
+
+```js
+setNumber(n => n + 1);
+setNumber(n => n + 1);
+setNumber(n => n + 1);
+```
+세 줄이 다 실행 된 후에 렌더링 됨.
+
+스냅샷 되어 들어가긴 하는데 렌더링 전에도 값은 내부적으로 변하고 있으니까 이러면 +3이 됨.
+
+입력한 순서대로 상태 변화 큐에 들어가고 순서대로 상태가 변한 뒤에 렌더링이 된다.
+
+
+```js
+setNumber(number + 5);
+setNumber(n => n + 1);
+// +6
+
+setNumber(n => n + 1);
+setNumber(number + 5);
+// +5
+```
+
+
+### Updating Objects in State
+
+Treat state as read-only 
+
+In other words, you should treat any JavaScript object that you put into state as read-only.
+
+불변성 (immutation)과 관련
+
+
+Write concise update logic with Immer 
+
+불변성 도와주는 라이브러리
+
+### Updating Arrays in State
+
+shallow copy할 때 nested 객체인 경우 조심
+
+
--- src/pages/ttt/index.tsx
+++ src/pages/ttt/index.tsx
@@ -1,35 +1,125 @@
+import { useState } from "react";
 import style from "./ttt.module.css";
 
 interface SquareProps {
-  text?: string;
+  value: string;
+  onClick: () => void;
 }
 function Square(props: SquareProps) {
-  const { text = "" } = props;
+  const { value = "", onClick } = props;
 
   return (
-    <button type="button" className={style.square}>
-      {text}
+    <button type="button" className={style.square} onClick={onClick}>
+      {value}
     </button>
   );
 }
 
+type boardValue = "" | "O" | "X";
+interface Log {
+  idx: number;
+  lastUser: boolean;
+  status: boardValue[];
+}
 function Board() {
+  const [board, setBoard] = useState<boardValue[]>(Array(9).fill(""));
+  const [currentPlayer, setCurrentPlayer] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const [log, setLog] = useState<Log[]>([]);
+
+  const getCurrentPlayer = (currentPlayer: boolean) => currentPlayer ? "O" : "X";
+
+  function isGameSet(board: boardValue[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return lines.some((oneLine) =>
+      board
+        .filter((_, i) => oneLine.includes(i))
+        .every((v) => v === (currentPlayer ? "O" : "X"))
+    );
+  }
+
+  function copyLog(logs: Log[]) {}
+
+  function onClickSquare(index: number) {
+    return () => {
+      if (isDone) return;
+      if (board[index] !== "") return;
+
+      const newBoard = board.map((b, i) =>
+        i === index ? getCurrentPlayer(currentPlayer) : b
+      );
+
+      const oldLog = log.map((l) => {
+        return { ...l };
+      });
+      setLog([
+        ...oldLog,
+        {
+          idx: log.length,
+          lastUser: currentPlayer,
+          status: board,
+        },
+      ]);
+      setBoard(newBoard);
+      setCurrentPlayer(!currentPlayer);
+      setIsDone(isGameSet(newBoard));
+    };
+  }
+
+  function revertToLog(targetLog: Log) {
+    return () => {
+      setCurrentPlayer(targetLog.lastUser);
+      setBoard(targetLog.status);
+      setLog(log.slice(0, targetLog.idx));
+      setIsDone(false);
+    };
+  }
+
   return (
     <>
       <div className={style["board-row"]}>
-        <Square text="1" />
-        <Square text="2" />
-        <Square text="3" />
+        <Square value={board[0]} onClick={onClickSquare(0)} />
+        <Square value={board[1]} onClick={onClickSquare(1)} />
+        <Square value={board[2]} onClick={onClickSquare(2)} />
       </div>
       <div className={style["board-row"]}>
-        <Square text="4" />
-        <Square text="5" />
-        <Square text="6" />
+        <Square value={board[3]} onClick={onClickSquare(3)} />
+        <Square value={board[4]} onClick={onClickSquare(4)} />
+        <Square value={board[5]} onClick={onClickSquare(5)} />
       </div>
       <div className={style["board-row"]}>
-        <Square text="7" />
-        <Square text="8" />
-        <Square text="9" />
+        <Square value={board[6]} onClick={onClickSquare(6)} />
+        <Square value={board[7]} onClick={onClickSquare(7)} />
+        <Square value={board[8]} onClick={onClickSquare(8)} />
+      </div>
+      {log.length > 0 && (
+        <div>
+          {log.map((targetLog, idx) => (
+            <div key={idx + 1}>
+              <button
+                type="button"
+                onClick={revertToLog(targetLog)}
+              >{`go to #${idx}`}</button>
+            </div>
+          ))}
+        </div>
+      )}
+      <div>
+        {isDone
+          ? `게임 끝. ${getCurrentPlayer(!currentPlayer)} 우승`
+          : `현재 차례: ${getCurrentPlayer(currentPlayer)}`}
       </div>
     </>
   );
